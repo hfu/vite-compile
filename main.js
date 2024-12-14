975f075c3990395a5e8f0e781a4bd40dd4c20594
Hidenori <hfu@users.noreply.github.com>
2024-12-14 19:53:23 +0900

    update

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import maplibregl from 'maplibre-gl'
 
 const map = new maplibregl.Map({
   container: 'map',
-  style: 'https://demotiles.maplibre.org/style.json',
+  style: 'https://tile.openstreetmap.jp/styles/osm-bright-ja/style.json',
   center: [139.6917, 35.6895],
   zoom: 8
 })
